eeaccad33d0628b844c9d4c080a3c9636fd35b45
Prince <princek2530@gmail.com>
2022-11-29 09:58:37 +0530

    Added: Single Player at Start Position 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    static int position = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulation Program");
+        System.out.println("Current Position:- " + position);
+
     }
 }
