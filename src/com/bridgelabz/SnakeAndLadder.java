f8d2bc5b22bf88c2287d3ec84761ca0f4cecf5e2
Prince <princek2530@gmail.com>
2022-11-29 10:06:57 +0530

    Added: Use RANDOM to get the dieNumber between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     static int position = 0;
+
+    public static void dieRoll() {
+        Random random = new Random();
+        int dieNumber = random.nextInt(6) + 1;
+        System.out.println("Die Number is:- " + dieNumber);
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulation Program");
         System.out.println("Current Position:- " + position);
-
+        dieRoll();
     }
 }
